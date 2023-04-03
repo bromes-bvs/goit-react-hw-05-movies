ed57e869a82918e1dc9070f8d3054323ac5e442a
Borys Supreme <bvs.bromes@gmail.com>
2023-04-04 01:00:06 +0300

    routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { fetchFilms } from 'components/service/tmdbAPI';
+// import { fetchFilms } from 'components/service/tmdbAPI';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-fetchFilms('reviews', 1, null, 823951);
+// fetchFilms('reviews', 1, null, 823951);
